ae0a28ca7371ec8ab0dbb1b8d060ef16d2d18da5
paulvongola <97293128+paulvongola@users.noreply.github.com>
2023-03-14 16:03:03 +0100

    update-score
    ajout du score

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+/*using UnityEngine.InputSystem;*/
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody _rigidbody;
+    private int ScoreValue = 0;
+    
+
+    [SerializeField] private TMP_Text _scoreText;
+    public delegate void MessageEvent();
+    public static event MessageEvent ObjetToucher;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _scoreText.text = "Score : " + ScoreValue;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            _rigidbody.AddForce(Input.GetAxis("Horizontal") * 0.5f, 0f, Input.GetAxis("Vertical"));
+        }
     }
+
+  /*  private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target_Trigger"))
+        {
+            Destroy(other.gameObject);
+            ObjetToucher.Invoke();
+
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target") || (collision.gameObject.CompareTag("Plateau")))
+        {
+            /*Instantiate(flame, collision.transform.position, Quaternion.identity);*/
+            Destroy(collision.gameObject);
+            UpdateScore(collision);
+            ObjetToucher.Invoke();
+        }
+    }
+    private void UpdateScore(Collision col)
+
+    {
+        if (col.gameObject.tag == "Target") // Si on touche un objet joyau , le score augmente de 1
+        {
+
+            ScoreValue++;
+        }
+        else
+        {
+            ScoreValue--;
+        }
+        _scoreText.text = "Score : " + ScoreValue;
+
+
+    }
+
+   
 }
+
